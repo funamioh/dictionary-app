6dc743ea89d15c323cd57fb402c82b2f7e69ba41
Miho F <85437491+funamioh@users.noreply.github.com>
2021-11-13 12:30:51 +0900

    Replaced logo and changed contents

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import logo from './prototype-logo.png';
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Dictionary
+        </h1>
       </header>
-    </div>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="text-center App-footer">This project was coded by Miho Funayama and is <a href="https://github.com/funamioh/dictionary-project">open-sourced on GitHub</a> and <a href="https://condescending-kepler-1383cc.netlify.app/">hosted by Netlify</a>.</footer>
+      </div>
   );
 }
 
