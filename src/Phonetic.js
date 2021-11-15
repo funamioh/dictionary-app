26de75b0b0099d20810a0ad5383a27a47a50cf50
Miho F <85437491+funamioh@users.noreply.github.com>
2021-11-15 19:03:09 +0900

    Added Phonetic icon button and url

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,5 +1,12 @@
 import React from "react";
+import logo from './headphones-alt-solid.svg';
+import './Phonetic.css';
 
 export default function Phonetic(props) {
-       return "Hello from Phonetic";
+    console.log(props.phonetic);
+        return (
+        <div className="Phonetic">
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
+            <img src={logo} className="Speaker-logo" alt="logo" /></a> </div>
+        )
    }
