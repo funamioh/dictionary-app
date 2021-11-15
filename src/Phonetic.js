f7986b1c30a427106bd7092ed8ec8b7dd424b09c
Miho F <85437491+funamioh@users.noreply.github.com>
2021-11-15 20:51:58 +0900

    Added phonetic icon style

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,5 +1,5 @@
 import React from "react";
-import logo from './headphones-alt-solid.svg';
+import logo from './volume-up-solid.svg';
 import './Phonetic.css';
 
 export default function Phonetic(props) {
@@ -7,6 +7,9 @@ export default function Phonetic(props) {
         return (
         <div className="Phonetic">
             <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-            <img src={logo} className="Speaker-logo" alt="logo" /></a> </div>
-        )
+            <img src={logo} className="Speaker-logo" alt="logo" /></a> 
+            <br />
+            {props.phonetic.text}
+            </div>
+        );
    }
