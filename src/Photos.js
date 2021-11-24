cfc22e5c2d23ccb90311538736e2d7943fdb4817
Miho F <85437491+funamioh@users.noreply.github.com>
2021-11-24 22:33:58 +0900

    Added function Photos to display image search

--- src/Photos.js
+++ src/Photos.js
@@ -2,8 +2,20 @@ import React from "react";
 
 export default function Photos(props) {
     if (props.photos) {
-        return <div className="Photos">
-        <h2>{props.photos.photos}</h2>
-        </div>
+        return (
+        <section className="Photos">
+            <div className="row">
+            {props.photos.map(function (photo, index) {
+                return (
+                <div className="col-4" key={index}>
+                <img src={photo.src.landscape} className="img-fluid" />
+                </div>
+                );
+                })}
+                </div>
+                </section>
+                );
+            } else {
+        return null;
     }
 }
