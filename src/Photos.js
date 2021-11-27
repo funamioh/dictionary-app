b0193565f80c82b68248d60e358d5b09e5d686b0
Miho F <85437491+funamioh@users.noreply.github.com>
2021-11-27 15:28:36 +0900

    Added styles to displayed photos

--- src/Photos.js
+++ src/Photos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
     if (props.photos) {
@@ -6,9 +7,11 @@ export default function Photos(props) {
         <section className="Photos">
             <div className="row">
             {props.photos.map(function (photo, index) {
+                console.log(photo);
                 return (
-                <div className="col-4" key={index}>
-                <img src={photo.src.landscape} className="img-fluid" />
+                <div className="col-3" key={index}>
+                    <a href={photo.src.original} target="_blank" rel="noreferrer">
+                <img src={photo.src.landscape} className="img-fluid" alt="dict-img" /></a>
                 </div>
                 );
                 })}
