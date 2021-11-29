ea5eb8bd20c52b4578315a01581230203c372af7
Miho F <85437491+funamioh@users.noreply.github.com>
2021-11-29 23:18:13 +0900

    Deleted speaker logo and replaced with text

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,15 +1,14 @@
 import React from "react";
-import logo from './volume-up-solid.svg';
 import './Phonetic.css';
 
 export default function Phonetic(props) {
     console.log(props.phonetic);
         return (
         <div className="Phonetic">
-            <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-            <img src={logo} className="Speaker-logo" alt="logo" /></a> 
-            <br />
+            <a href={props.phonetic.audio} target="_blank" rel="noreferrer" className="phonetic-button">Listen📣</a> 
+            <span className="text">
             {props.phonetic.text}
+            </span>
             </div>
         );
    }
